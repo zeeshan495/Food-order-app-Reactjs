9a9bdd16a080432aa17315cf34b8ee23b0dbbdcf
zeeshan495 <shaikzeeshan88@gmail.com>
2025-08-27 13:28:13 +0530

    Adding common component button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import logoImg from '../assets/logo.jpg';
+import Button from './UI/Button';
 
 export default function Header() {
-    return (
-        <header id='main-header'>
-            <div id='title'>  
-                <img src={logoImg} alt='A restaurant'></img>
-                <h1>Food Order App in React</h1>
-            </div>
-            <nav>
-                <button>Cart (0)</button>
-            </nav>
-        </header>
-    );
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A restaurant"></img>
+        <h1>Food Order App in React</h1>
+      </div>
+      <nav>
+        <Button textOnly={true}>Cart (0)</Button>
+      </nav>
+    </header>
+  );
 }
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,6 @@
+import { currencyFormatter } from '../util/formatting';
+import Button from './UI/Button';
+
 export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
@@ -5,11 +8,13 @@ export default function MealItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt="A meal"></img>
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
